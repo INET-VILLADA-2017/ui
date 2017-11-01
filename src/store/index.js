b69952d0722e899342b578d8e43e63118b96cb0c
Unknown <franpitri@gmail.com>
2017-11-01 12:17:08 -0300

    persist redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/es/storage'
 import AppReducer from '../reducers'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth', 'data'],
+}
+
+const reducer = persistReducer(persistConfig, AppReducer)
+
 export default function configureStore() {
-    return createStore(
-        AppReducer,
+    let store = createStore(
+        reducer,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         applyMiddleware(
             thunk
         )
     )
+    let persistor = persistStore(store)
+    return {persistor, store}
 }
